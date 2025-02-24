ef5b59ad10f72963aaa512ee2c0fce6c8b3733e2
Sander Fieten <sander@holodeck-b2b.org>
2025-02-24 11:43:00 +0100

    Refactoring to resolve cyclic dependency

--- modules/holodeckb2b-it/src/test/java/org/holodeckb2b/test/integration/IntegrationTest.java
+++
@@ -1,8 +0,0 @@
-package org.holodeckb2b.test.integration;
-
-/**
- * Category for junit integration testing
- *
- * @author Timur Shakuov (t.shakuov at gmail.com)
- */
-public interface IntegrationTest {}
